b2ef72640568ce0397d738c1a5f3fbd69a3a9bf8
restumi <maulanaismael5@gmail.com>
2025-07-28 14:40:23 +0700

    add route regist, login, and logout

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,15 @@
 use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/register', [AuthController::class], 'register');
-Route::post('/login', [AuthController::class], 'login');
+Route::get('/test', function() {
+    return response()->json([
+        'message' => 'api is ready'
+    ]);
+});
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group( function() {
-    Route::post('/logout', [AuthController::class], 'logout');
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
